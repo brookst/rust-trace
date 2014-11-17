27974711d0e2cae2d87a45dfa14917839907d202
Tim Brooks <brooks@cern.ch>
2014-11-17 09:20:01 +0000

    Add doc comments

--- src/shape/mod.rs
+++ src/shape/mod.rs
@@ -1,8 +1,11 @@
 //! Shapes to be used in the scene.
 use point::Point;
 
+/// Trait for scene objects
 pub trait Shape<T: Float> {
-    fn intersect(&self, Point<T>) -> Option<T>;
+    /// Intersection routine, returns distance along ray
+    fn intersect(&self, ray: Point<T>) -> Option<T>;
 }
 
+/// Export shapes
 pub mod sphere;
